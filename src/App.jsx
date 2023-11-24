6ca98f8c0c6d34277bebaa1819208ab965e7f2e3
GRPS GANEGODA <piumisarangag@gmail.com>
2023-11-24 23:29:29 +0530

    create navigation bar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Components/Nav';
+
+const App = () => {
+  return (
+    <div className="bg-slate-900">
+      <Nav />
+    </div>
+  );
+};
+
+
+export default App;
